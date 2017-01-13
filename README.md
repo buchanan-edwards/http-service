442043b424b5e44205a2b3d848583d781306a751
Frank Hellwig <frank@hellwig.org>
2017-01-12 19:58:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,125 @@
+# http-service
+
+A wrapper around the node http or https request method.
+
+Version 1.0.0
+
+## 1. Installation
+
+```bash
+$ npm install --save @be/http-service
+```
+
+## 2. Usage
+
+```javascript
+const HttpService = require('@be/http-service');
+
+const service = new HttpService('http://httpbin.org');
+
+service.get('get', (err, body) => {
+    console.log(body);
+}
+```
+
+## 3. API
+
+The package exports the `HttpService` class. This class provides the `request` method and various convenience methods that call the `request` method. The `request` method can be overridden in a subclass. One reason, for example, is adding header values. Be sure to call `super.request(...)` when overriding this method.
+
+### 3.1 constructor
+
+```javascript
+HttpService(uri)
+```
+
+Creates a new `HttpService` instance for the specified `uri`. The `uri` is parsed and the protocol, hostname, port, and pathname are extracted. The protocol is required and must be either `http` or `https`. If the port is not specified, it will default to port 80 for `http` requests and port 443 for `https` requests. The pathname is prepended to all requests and will be a `/` if not specified. Therefore, do not prefix any request path with an additional `/`.
+
+**Examples:**
+
+```javascript
+const service = new HttpService('http://example.com');
+```
+
+```javascript
+const service = new HttpService('https://example.com:9000');
+```
+
+### 3.2 request
+
+```javascript
+service.request(method, path, headers, data, callback)
+```
+
+Sends an HTTP or an HTTPS request to the host specified in the constructor.
+
+- The `method` should be one of `GET`, `HEAD`, `OPTIONS`, `TRACE`, `POST`, `PUT`, `PATCH` or `DELETE`. The method is converted to upper case.
+- The `path` identifies the resource with respect to the URI specified in the constructor. Do not prefix this path with a `/`.
+- The `headers` must be an object or `null`.
+- The `data` specifies the message body and can be a `Buffer`, a string, or an object. If the `data` parameter is an object, then it is processed as follows:
+    - If the `Content-Type` header is `application/json`, then the data is serialized by calling `JSON.stringify`.
+    - If the `Content-Type` header is `application/x-www-form-urlencoded`, then the data is serialized by calling `querystring.stringify`.
+    - If the `Content-Type` header is not set, then the data is serialized by calling `JSON.stringify` and the `Content-Type` header is set to  `application/json`.
+- The `callback` is called upon completion of the request. On error, it is called as `callback(err)`. On success, it is called as `callback(null, data, type, headers)`.
+    - The `data` is the response message body. It will be a string if the `type` begins with `text` or ends with `+xml`. It will be an object if the `type` is `application/json`. Otherwise, the `data` will be a `Buffer`.
+    - The `type` is the value of the `Content-Type` header with all parameters removed. For example, if the `Content-Type` is `text/html; charset=utf-8`, then the `type` argument in the callback will simply be `text/html`.
+    - The `headers` are the full, unmodified headers from the response.
+
+### 3.3 get, head
+
+```javascript
+service.get(path, [query,] callback)
+service.head(path, [query,] callback)
+```
+
+Convenience methods for `GET` and `HEAD` requests. If the optional `query` object is specified, it is serialized and appended to the `path` preceded by a `?`. If the `path` already contains a query, then it is appended with a `&`.
+
+### 3.4 post, put, patch
+
+```javascript
+service.post(path, data, callback)
+service.put(path, data, callback)
+service.patch(path, data, callback)
+```
+
+Convenience methods for the `POST`, `PUT`, and `PATCH` requests.
+
+### 3.5 delete
+
+```javascript
+service.delete(path, callback)
+```
+
+Convenience method for the `DELETE` request.
+
+### 3.6 Media Types
+
+There are two static media type constants on the HttpService class:
+
+```javascript
+HttpService.JSON_MEDIA_TYPE = 'application/json';
+HttpService.FORM_MEDIA_TYPE = 'application/x-www-form-urlencoded';
+```
+
+## 4. License
+
+MIT License
+
+Copyright (c) 2017 Buchanan & Edwards
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ http-service.js
@@ -0,0 +1,248 @@
+//==============================================================================
+// Exports the HttpService class that sends HTTP or HTTPS requests to a server.
+//==============================================================================
+// Copyright (c) 2017 Buchanan & Edwards
+//==============================================================================
+
+'use strict';
+
+//------------------------------------------------------------------------------
+// Dependencies
+//------------------------------------------------------------------------------
+
+const http = require('http');
+const https = require('https');
+const HttpError = require('@be/http-error');
+const url = require('url');
+const util = require('util');
+const querystring = require('querystring');
+
+//------------------------------------------------------------------------------
+// Initialization
+//------------------------------------------------------------------------------
+
+const JSON_MEDIA_TYPE = 'application/json';
+const FORM_MEDIA_TYPE = 'application/x-www-form-urlencoded';
+
+const CONTENT_TYPE_HEADER = 'content-type';
+const CONTENT_LENGTH_HEADER = 'content-length';
+
+const slice = Array.prototype.slice;
+
+//------------------------------------------------------------------------------
+// Private
+//------------------------------------------------------------------------------
+
+function httpError(code, opt, msg) {
+    msg = `[${opt.method} ${opt.protocol}//${opt.host}:${opt.port}${opt.path}] ${msg}`;
+    return new HttpError(code, msg);
+}
+
+function appendQuery(path, query) {
+    if (util.isObject(query)) {
+        query = querystring.stringify(query);
+    }
+    if (util.isString(query)) {
+        let sep = (path.indexOf('?') < 0) ? '?' : '&';
+        return path + sep + query;
+    }
+    return path;
+}
+
+function headerValue(headers, name) {
+    if (util.isObject(headers)) {
+        let keys = Object.keys(headers);
+        for (let i = 0, n = keys.length; i < n; i++) {
+            let key = keys[i];
+            if (key.toLowerCase() === name) {
+                return headers[key];
+            }
+        }
+    }
+    return null;
+}
+
+function removeParams(value) {
+    if (value) {
+        let semi = value.indexOf(';');
+        if (semi > 0) {
+            return value.substring(0, semi);
+        }
+    }
+    return value;
+}
+
+//------------------------------------------------------------------------------
+// Public
+//------------------------------------------------------------------------------
+
+class HttpService {
+
+    /**
+     * Accepts an HTTP or HTTPS URI (e.g., https://example.com:443).
+     */
+    constructor(uri) {
+        const parsed = url.parse(uri);
+        if (parsed.protocol !== 'http:' && parsed.protocol !== 'https:') {
+            throw new URIError(`'${uri}' invalid protocol (expected http or https)`);
+        }
+        this.protocol = parsed.protocol;
+        this.hostname = parsed.hostname;
+        this.pathname = parsed.pathname;
+        this.port = parseInt(parsed.port);
+        if (!this.port) {
+            this.port = this.protocol === 'http:' ? 80 : 443;
+        }
+    }
+
+    // get(path, [query,] callback)
+    // callback(err, body, type);
+    get(path, query, callback) {
+        if (typeof query === 'function') {
+            callback = query;
+            query = null;
+        } else {
+            path = appendQuery(path, query);
+        }
+        this.request('GET', path, null, null, callback);
+    }
+
+    head(path, query, callback) {
+        if (typeof query === 'function') {
+            callback = query;
+            query = null;
+        } else {
+            path = appendQuery(path, query);
+        }
+        this.request('HEAD', path, null, null, callback);
+    }
+
+    post(path, data, callback) {
+        this.request('POST', path, null, data, callback);
+    }
+
+    put(path, data, callback) {
+        this.request('PUT', path, null, data, callback);
+    }
+
+    patch(path, data, callback) {
+        this.request('PATCH', path, null, data, callback);
+    }
+
+    delete(path, callback) {
+        this.request('DELETE', path, null, null, callback);
+    }
+
+    request(method, path, headers, data, callback) {
+        const client = this.protocol === 'http:' ? http : https;
+        method = method.toUpperCase();
+        headers = headers || {};
+        if (data !== null) {
+            if (util.isObject(data) && !Buffer.isBuffer(data)) {
+                let type = headerValue(headers, CONTENT_TYPE_HEADER);
+                switch (type) {
+                    case JSON_MEDIA_TYPE:
+                        data = JSON.stringify(data);
+                        break;
+                    case FORM_MEDIA_TYPE:
+                        data = querystring.stringify(data);
+                        break;
+                    case null:
+                        headers[CONTENT_TYPE_HEADER] = JSON_MEDIA_TYPE;
+                        data = JSON.stringify(data);
+                        break;
+                    default:
+                        throw new Error('Unsuported media type (cannot serialize object): ' + type);
+                }
+            }
+            if (util.isString(data) && headerValue(headers, CONTENT_LENGTH_HEADER) === null) {
+                headers[CONTENT_LENGTH_HEADER] = Buffer.byteLength(data);
+            }
+        }
+        let options = {
+            method: method,
+            protocol: this.protocol,
+            host: this.hostname,
+            port: this.port,
+            path: this.pathname + (path || ''),
+            headers: headers
+        };
+        let chunks = [];
+        let request = client.request(options, response => {
+            response.on('data', chunk => {
+                chunks.push(chunk);
+            });
+            response.on('end', _ => {
+                if (response.statusCode === 204) {
+                    return callback(null, null, null, response.headers);
+                }
+                let code = response.statusCode;
+                let type = removeParams(headerValue(response.headers, CONTENT_TYPE_HEADER));
+                let body = Buffer.concat(chunks);
+                if (method === 'HEAD') {
+                    body = null;
+                } else if (type === 'application/json') {
+                    body = body.toString();
+                    if (!body) {
+                        return callback(httpError(502, options, 'Empty Response'));
+                    }
+                    try {
+                        body = JSON.parse(body);
+                    } catch (e) {
+                        return callback(httpError(502, options, e.message));
+                    }
+                    // Sometimes Microsoft returns an error description.
+                    if (body.error_description) {
+                        let message = body.error_description.split(/\r?\n/)[0];
+                        return callback(httpError(code, options, message));
+                    }
+                    // Other times Microsoft returns an error object.
+                    if (body.error && body.error.message) {
+                        return callback(httpError(code, options, body.error.message));
+                    }
+                    // It could be an odata error.
+                    if (body['odata.error'] && body['odata.error'].message) {
+                        return callback(httpError(code, options, body['odata.error'].message.value));
+                    }
+                } else if (type.startsWith('text/') || type.endsWith('+xml')) {
+                    body = body.toString();
+                }
+                let success = code === 200 || code === 201;
+                if (!success) {
+                    return callback(httpError(response.statusCode, options,
+                        'The request was not successfully completed.'));
+                }
+                callback(null, body, type, response.headers);
+            });
+        });
+        request.on('error', err => {
+            callback(err);
+        });
+        if (data !== null) {
+            if (util.isString(data) || Buffer.isBuffer(data)) {
+                request.write(data);
+            } else {
+                throw new Error('Invalid request data (must be string or Buffer): ' + (typeof data));
+            }
+        }
+        request.end();
+    }
+}
+
+HttpService.JSON_MEDIA_TYPE = JSON_MEDIA_TYPE;
+HttpService.FORM_MEDIA_TYPE = FORM_MEDIA_TYPE;
+
+//------------------------------------------------------------------------------
+// Exports
+//------------------------------------------------------------------------------
+
+module.exports = HttpService;
+
+let service = new HttpService('http://httpbin.org/x/');
+service.get('get', function (err, data) {
+    if (err) {
+        console.error(err.toString());
+    } else {
+        console.log(data);
+    }
+})
